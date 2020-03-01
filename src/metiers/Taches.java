a9740fdb46366d614a795c7a81697895f409964f
BuildTools <unconfigured@null.spigotmc.org>
2020-03-01 16:28:15 +0100

    Changement 1.3

--- src/metiers/Taches.java
+++ src/metiers/Taches.java
@@ -1,6 +1,7 @@
 package metiers;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Taches {
 
@@ -10,9 +11,7 @@ public class Taches {
     private Date date;
     private String status;
     private String resolution;
-
-    //private ArrayList<Taches> listTask = new ArrayList<>();
-
+    
     public Taches(int id, String createur, String description, Date date, String status, String resolution){
         this.id = id;
         this.createur = createur;
@@ -22,5 +21,32 @@ public class Taches {
         this.resolution = resolution;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    @Override
+    public String toString() {
+        return "Taches{" +
+                "id=" + id +
+                ", createur='" + createur + '\'' +
+                ", description='" + description + '\'' +
+                ", date=" + date +
+                ", status='" + status + '\'' +
+                ", resolution='" + resolution + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Taches taches = (Taches) o;
+        return id == taches.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
