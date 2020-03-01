c2e67c6cf88254137a68abd149f2e8019c3864aa
BuildTools <unconfigured@null.spigotmc.org>
2020-03-01 16:18:47 +0100

    Changement 1.1

--- src/domaine/IGestionTaches.java
+++ src/domaine/IGestionTaches.java
@@ -1,4 +1,12 @@
 package domaine;
 
+import java.util.Date;
+
 public interface IGestionTaches {
+
+    void createTask(int id, String createur, String description, Date date, String status, String resolution);
+    void cancelTask(int id, String status);
+    void dateUpdateTask(int id, Date date);
+    void seeTask(int id);
+
 }
--- src/metiers/Taches.java
+++ src/metiers/Taches.java
@@ -1,4 +1,14 @@
 package metiers;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Taches {
+
+    private int id;
+    private String createur;
+    private String description;
+    private 
+
+    private ArrayList<Taches> listTask = new ArrayList<>();
 }
